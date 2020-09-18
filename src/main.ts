2f93cfc0fb476d15453b330bd33f82a426c2debf
tran thanh quang <quang.tran290699@gmail.com>
2020-09-18 11:44:14 +0700

    config swagger API document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import path = require('path');
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+require('dotenv').config({
+  path: path.resolve(process.cwd(), process.env.NODE_ENV + '.env'),
+});
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('Booking Hotel Document')
+    .setDescription('The Booking Hotel API description')
+    .setVersion('1.0')
+    .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  Logger.log(`Listening on Port: ${process.env.PORT || 3001}`);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
