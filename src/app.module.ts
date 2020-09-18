9f3933391e99ae762b54b1659f750ab0eb72ca69
tran thanh quang <quang.tran290699@gmail.com>
2020-09-18 11:45:12 +0700

    config database postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './shared/auth/auth.module';
+import { ConfigModule } from './shared/config/config.module';
+import { ConfigService } from './shared/config/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const {host, port, name, password, user} = configService.databaseConfig;
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username: user,
+          password,
+          database: name,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        };
+      },
+    }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
