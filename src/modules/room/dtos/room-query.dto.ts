f6b8da6c0a069cc339287d221c8125ab2e73f077
tran thanh quang <quang.tran290699@gmail.com>
2020-10-30 16:28:55 +0700

    fix: dto room

--- src/modules/room/dtos/room-query.dto.ts
+++ src/modules/room/dtos/room-query.dto.ts
@@ -1,6 +1,6 @@
 import { ApiModelPropertyOptional } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
-import { IsDate, IsNumber, IsOptional } from "class-validator";
+import { IsDate, IsNumber, IsOptional, IsString } from "class-validator";
 import { AQuery } from "src/shared/classes/query.dto";
 
 export class RoomQueryDto extends AQuery {
@@ -9,6 +9,11 @@ export class RoomQueryDto extends AQuery {
   @Transform(v => new Number(v))
   @IsNumber()
   hotelId: number;
+
+  @ApiModelPropertyOptional()
+  @IsOptional()
+  @IsString()
+  name: string;
 }
 
 export class RoomTransactionQueryDto extends AQuery {
